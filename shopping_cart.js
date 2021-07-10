b6bfe282a7c52b3a1fea0fc2e895f681a6c093d5
Paul-Nixon <nixonp4@gmail.com>
2021-07-09 19:06:12 -0500

    started working on shopping_cart.js

---
+++ shopping_cart.js
@@ -0,0 +1,43 @@
+if (document.readyState === "loading")
+{
+    document.addEventListener("DOMContentLoaded", ready)
+}
+else
+{
+    ready();
+}
+
+
+function ready()
+{
+    //
+    if (sessionStorage.getItem("cart") == null)
+    {
+        //
+        sessionStorage.setItem("cart", "");
+    }
+    else
+    {
+        //
+    }
+
+    //
+    document.querySelectorAll(".btn-add-to-cart").addEventListener("click", addToCart);
+}
+
+
+function addToCart(event)
+{
+    // 
+    let itemImg = event.target.parentElement.querySelector(".shop-item-img").style.backgroundImage;
+    let itemName = event.target.parentElement.querySelector(".item-title").innerText;
+    let itemPrice = event.target.parentElement.querySelector(".item-price").innerText;
+
+    //
+}
+
+
+function clearCart()
+{
+    // 
+}
