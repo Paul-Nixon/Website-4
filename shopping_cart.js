1123a41702ea42dafa7525a1a14cc1e18a7b8477
Paul-Nixon <nixonp4@gmail.com>
2021-07-10 19:06:40 -0500

    working on shopping_cart.js x2

--- shopping_cart.js
+++ shopping_cart.js
@@ -14,7 +14,7 @@ function ready()
     if (sessionStorage.getItem("cart") == null)
     {
         //
-        sessionStorage.setItem("cart", "");
+        sessionStorage.setItem("cart", JSON.stringify({}));
     }
     else
     {
@@ -34,12 +34,39 @@ function addToCart(event)
     let itemImg = event.target.parentElement.querySelector(".shop-item-img").style.backgroundImage;
     let itemName = event.target.parentElement.querySelector(".item-title").innerText;
     let itemPrice = event.target.parentElement.querySelector(".item-price").innerText;
+    let newItem = 
+    {
+        image: itemImg,
+        name: itemName,
+        price: itemPrice
+    };
 
     //
+    let oldCart = JSON.parse(sessionStorage.getItem("cart"));
+    let newItemKeyName = "item" + (Object.keys(oldCart).length + 1);
+    let newCart = Object.assign(oldCart, {newItemKeyName: newItem});
+    sessionStorage.setItem("cart", JSON.stringify(newCart));
 }
 
 
 function clearCart()
 {
-    // 
+    sessionStorage.removeItem("cart");
+}
+
+
+function itemExistsInCart(itemName)
+{
+    //
+    if (Object.keys(JSON.parse(sessionStorage.getItem("cart"))).length == 0)
+    {
+        return false;
+    }
+    else
+    {
+        //
+        Object.keys(JSON.parse(sessionStorage.getItem("cart"))).forEach(item => {
+
+        });
+    }
 }
