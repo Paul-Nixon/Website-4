950fea99e85888c0446c3caad1561dd7664dab5c
Paul-Nixon <nixonp4@gmail.com>
2021-07-20 19:09:53 -0500

    working on cart_webpage.html

--- cart_webpage.js
+++ cart_webpage.js
@@ -11,32 +11,61 @@ function ready()
 {
     if (sessionStorage.getItem("cart") == null || JSON.parse(sessionStorage.getItem("cart")).length === 0)
     {
-        renderEmptyCartMessage();
+        document.querySelector(".cart-items-wrapper").style.display = "none";
+        document.querySelector(".subtotal-and-checkout-btn-wrapper").style.display = "none";
     }
     else
     {
+        document.querySelector(".cart-message-and-shop-links-wrapper").style.display = "none";
         renderCartItems();
     }
 }
 
 
-function renderEmptyCartMessage()
+function renderCartItems()
 {
-    const mainContentContainer = document.querySelector(".cart-main-content");
+    const cartItems = JSON.parse(sessionStorage.getItem("cart"));
 
-    mainContentContainer.innerHTML = `
-        <span class="cart-message">There are no items in your cart.</span>
-        
-        <div class="shop-links-wrapper">
-            <button class="btn btn-shop-link">men's shop</button>
-            <button class="btn btn-shop-link">women's shop</button>
-        </div>`;
+    for (const cartItem of cartItems)
+    {
+        const cartItemContainer = document.createElement("div");
+        cartItemContainer.classList.add("cart-item");
+        cartItemContainer.innerHTML = `
+            <img class="img cart-webpage-img" src="${cartItem.image}">
+
+            <div class="item-info-wrapper">
+                <h3 class="item-title">${cartItem.name}</h3>
+                <span class="item-price">$${cartItem.price}</span>
+                <input type="number" min="0" value="${cartItem.quantity}" class="item-quantity">
+            </div>`;
+        cartItemContainer.querySelector(".item-quantity").onchange = (event) => {
+            if (event.target.value == 0)
+            {
+                removeCartItem(event);
+            }
+            else
+            {
+                event.target.closest(".cart-item").querySelector(".item-price").innerText = "$" +
+                (parseFloat(cartItem.price) * event.target.value).toFixed(2);
+                updateItemQuantity(event);
+            }
+        };
+    }
 }
 
 
-function renderCartItems()
+function updateItemQuantity(event)
 {
-    const mainContentContainer = document.querySelector(".cart-main-content");
+    const cartItems = JSON.parse(sessionStorage.getItem("cart")),
+    cartItemName = event.target.previousElementSibling.previousElementSibling.innerText;
 
-    
+    for (const cartItem of cartItems)
+    {
+        if (cartItem.name === cartItemName)
+        {
+            cartItem.quantity = event.target.value;
+            sessionStorage.setItem("cart", JSON.stringify(cartItems));
+            break;
+        }
+    }
 }
